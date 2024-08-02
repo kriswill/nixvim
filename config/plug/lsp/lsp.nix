e31bfcfef2c9fb660c23d80531d2a6b1c84a9369
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-08-01 21:55:11 -0700

    add "vim" to lua-ls globals

--- config/plug/lsp/lsp.nix
+++ config/plug/lsp/lsp.nix
@@ -6,7 +6,12 @@
       servers = {
         eslint = { enable = true; };
         html = { enable = true; };
-        lua-ls = { enable = true; };
+        lua-ls = {
+          enable = true;
+          extraOptions = {
+            diagnostics.globals = [ "vim" ];
+          };
+        };
         nil-ls = { enable = true; };
         marksman = { enable = true; };
         pyright = { enable = true; };
