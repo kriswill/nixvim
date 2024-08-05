b6eb71bf6e5ecce7b5cb7b33c0b1a0696827d1ce
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-08-04 20:33:43 -0700

    cleanup

--- config/keys.nix
+++ config/keys.nix
@@ -8,61 +8,51 @@
       key = "<leader>f";
       action = "+find/file";
     }
-
     {
       mode = "n";
       key = "<leader>s";
       action = "+search";
     }
-
     {
       mode = "n";
       key = "<leader>q";
       action = "+quit/session";
     }
-
     {
       mode = [ "n" "v" ];
       key = "<leader>g";
       action = "+git";
     }
-
     {
       mode = "n";
       key = "<leader>u";
       action = "+ui";
     }
-
     {
       mode = "n";
       key = "<leader>w";
       action = "+windows";
     }
-
     {
       mode = "n";
       key = "<leader><Tab>";
       action = "+tab";
     }
-
     {
       mode = [ "n" "v" ];
       key = "<leader>d";
       action = "+debug";
     }
-
     {
       mode = [ "n" "v" ];
       key = "<leader>c";
       action = "+code";
     }
-
     {
       mode = [ "n" "v" ];
       key = "<leader>t";
       action = "+test";
     }
-
     # Tabs
     {
       mode = "n";
@@ -73,7 +63,6 @@
         desc = "New Tab";
       };
     }
-
     {
       mode = "n";
       key = "<leader><tab>d";
@@ -83,7 +72,6 @@
         desc = "Close tab";
       };
     }
-
     # Windows
     {
       mode = "n";
@@ -94,7 +82,6 @@
         desc = "Other window";
       };
     }
-
     {
       mode = "n";
       key = "<leader>wd";
@@ -104,7 +91,6 @@
         desc = "Delete window";
       };
     }
-
     {
       mode = "n";
       key = "<leader>w-";
@@ -114,7 +100,6 @@
         desc = "Split window below";
       };
     }
-
     {
       mode = "n";
       key = "<leader>w|";
@@ -124,7 +109,6 @@
         desc = "Split window right";
       };
     }
-
     {
       mode = "n";
       key = "<C-h>";
@@ -134,7 +118,6 @@
         desc = "Move to window left";
       };
     }
-
     {
       mode = "n";
       key = "<C-l>";
@@ -144,7 +127,6 @@
         desc = "Move to window right";
       };
     }
-
     {
       mode = "n";
       key = "<C-k>";
@@ -154,7 +136,6 @@
         desc = "Move to window over";
       };
     }
-
     {
       mode = "n";
       key = "<C-j>";
@@ -164,7 +145,6 @@
         desc = "Move to window bellow";
       };
     }
-
     {
       mode = "n";
       key = "<C-s>";
@@ -174,7 +154,6 @@
         desc = "Save file";
       };
     }
-
     # Quit/Session
     {
       mode = "n";
@@ -185,7 +164,6 @@
         desc = "Quit all";
       };
     }
-
     {
       mode = "n";
       key = "<leader>ul";
@@ -195,7 +173,6 @@
         desc = "Toggle Line Numbers";
       };
     }
-
     {
       mode = "n";
       key = "<leader>uL";
@@ -205,7 +182,6 @@
         desc = "Toggle Relative Line Numbers";
       };
     }
-
     {
       mode = "n";
       key = "<leader>uw";
@@ -215,184 +191,157 @@
         desc = "Toggle Line Wrap";
       };
     }
-
     {
       mode = "v";
       key = "J";
       action = ":m '>+1<CR>gv=gv";
-      options = { desc = "Use move command when line is highlighted "; };
+      options.desc = "Use move command when line is highlighted ";
     }
-
     {
       mode = "v";
       key = "K";
       action = ":m '>-2<CR>gv=gv";
-      options = { desc = "Use move command when line is highlighted "; };
+      options.desc = "Use move command when line is highlighted ";
     }
-
     {
       mode = "n";
       key = "J";
       action = "mzJ`z";
-      options = {
-        desc = "Allow cursor to stay in the same place after appending to current line ";
-      };
+      options.desc = "Allow cursor to stay in the same place after appending to current line ";
     }
-
     {
       mode = "n";
       key = "<C-d>";
       action = "<C-d>zz";
-      options = {
-        desc = "Allow C-d and C-u to keep the cursor in the middle";
-      };
+      options.desc = "Allow C-d and C-u to keep the cursor in the middle";
     }
-
     {
       mode = "n";
       key = "<C-u>";
       action = "<C-u>zz";
-      options = {
-        desc = "Allow C-d and C-u to keep the cursor in the middle";
-      };
+      options.desc = "Allow C-d and C-u to keep the cursor in the middle";
     }
-
     {
       mode = "n";
       key = "n";
       action = "nzzzv";
-      options = { desc = "Allow search terms to stay in the middle "; };
+      options.desc = "Allow search terms to stay in the middle ";
     }
-
     {
       mode = "n";
       key = "N";
       action = "Nzzzv";
-      options = { desc = "Allow search terms to stay in the middle "; };
+      options.desc = "Allow search terms to stay in the middle ";
     }
-
     # Paste stuff without saving the deleted word into the buffer
     {
       mode = "x";
       key = "<leader>p";
       action = ''"_dP'';
-      options = { desc = "Deletes to void register and paste over"; };
+      options.desc = "Deletes to void register and paste over";
     }
-
     # Copy stuff to system clipboard with <leader> + y or just y to have it just in vim
     {
       mode = [ "n" "v" ];
       key = "<leader>y";
       action = ''"+y'';
-      options = { desc = "Copy to system clipboard"; };
+      options.desc = "Copy to system clipboard";
     }
-
     {
       mode = [ "n" "v" ];
       key = "<leader>Y";
       action = ''"+Y'';
-      options = { desc = "Copy to system clipboard"; };
+      options.desc = "Copy to system clipboard";
     }
-
     # Delete to void register
     {
       mode = [ "n" "v" ];
       key = "<leader>D";
       action = ''"_d'';
-      options = { desc = "Delete to void register"; };
+      options.desc = "Delete to void register";
     }
-
     # <C-c> instead of pressing esc just because
     {
       mode = "i";
       key = "<C-c>";
       action = "<Esc>";
     }
-
     {
       mode = "n";
       key = "<leader>m";
       action = "<CMD> Grapple toggle <CR>";
-      options = { desc = "Grapple Toggle tag"; };
+      options.desc = "Grapple Toggle tag";
     }
-
     {
       mode = "n";
       key = "<leader>k";
       action = "<CMD> Grapple toggle_tags <CR>";
-      options = { desc = "Grapple Toggle tag"; };
+      options.desc = "Grapple Toggle tag";
     }
-
     {
       mode = "n";
       key = "<leader>K";
       action = "<CMD> Grapple toggle_scopes <CR>";
-      options = { desc = "Grapple Toggle scopes"; };
+      options.desc = "Grapple Toggle scopes";
     }
-
     {
       mode = "n";
       key = "<leader>j";
       action = "<CMD> Grapple cycle forward <CR>";
-      options = { desc = "Grapple Cycle forward"; };
+      options.desc = "Grapple Cycle forward";
     }
-
     {
       mode = "n";
       key = "<leader>J";
       action = "<CMD> Grapple cycle backward <CR>";
-      options = { desc = "Grapple Cycle backward"; };
+      options.desc = "Grapple Cycle backward";
     }
-
     {
       mode = "n";
       key = "<leader>1";
       action = "<CMD> Grapple select index=1<CR>";
-      options = { desc = "Grapple Select 1"; };
+      options.desc = "Grapple Select 1";
     }
-
     {
       mode = "n";
       key = "<leader>2";
       action = "<CMD> Grapple select index=2<CR>";
-      options = { desc = "Grapple Select 2"; };
+      options.desc = "Grapple Select 2";
     }
-
     {
       mode = "n";
       key = "<leader>3";
       action = "<CMD> Grapple select index=3<CR>";
-      options = { desc = "Grapple Select 3"; };
+      options.desc = "Grapple Select 3";
     }
-
     {
       mode = "n";
       key = "<leader>4";
       action = "<CMD> Grapple select index=4<CR>";
-      options = { desc = "Grapple Select 4"; };
+      options.desc = "Grapple Select 4";
     }
   ];
-  extraConfigLua = ''
+  extraConfigLua = /*lua*/''
     function ToggleLineNumber()
-    if vim.wo.number then
-      vim.wo.number = false
-    else
-      vim.wo.number = true
+      if vim.wo.number then
+        vim.wo.number = false
+      else
+        vim.wo.number = true
         vim.wo.relativenumber = false
-        end
-        end
+      end
+    end
 
-        function ToggleRelativeLineNumber()
-        if vim.wo.relativenumber then
-          vim.wo.relativenumber = false
-        else
-          vim.wo.relativenumber = true
-            vim.wo.number = false
-            end
-            end
+    function ToggleRelativeLineNumber()
+      if vim.wo.relativenumber then
+        vim.wo.relativenumber = false
+      else
+        vim.wo.relativenumber = true
+        vim.wo.number = false
+      end
+    end
 
-            function ToggleWrap()
-            vim.wo.wrap = not vim.wo.wrap
-            end
+    function ToggleWrap()
+      vim.wo.wrap = not vim.wo.wrap
+    end
   '';
 }
--- config/plug/ui/telescope.nix
+++ config/plug/ui/telescope.nix
@@ -13,162 +13,114 @@
       defaults = {
         layout_config = {
           horizontal = {
-            prompt_position = "top";
+            prompt_position = "bottom";
           };
         };
         sorting_strategy = "ascending";
+        set_env.COLORTERM = "truecolor";
+        borderchars = [ "▔" "▕" "▁" "▏" "🭽" "🭾" "🭿" "🭼" ];
       };
     };
     keymaps = {
       "<leader><space>" = {
         action = "find_files";
-        options = {
-          desc = "Find project files";
-        };
+        options.desc = "Find project files";
       };
       "<leader>/" = {
         action = "live_grep";
-        options = {
-          desc = "Grep (root dir)";
-        };
+        options.desc = "Grep (root dir)";
       };
       "<leader>:" = {
         action = "command_history";
-        options = {
-          desc = "Command History";
-        };
+        options.desc = "Command History";
       };
       "<leader>b" = {
         action = "buffers";
-        options = {
-          desc = "+buffer";
-        };
+        options.desc = "+buffer";
       };
       "<leader>ff" = {
         action = "find_files";
-        options = {
-          desc = "Find project files";
-        };
+        options.desc = "Find project files";
       };
       "<leader>fr" = {
         action = "live_grep";
-        options = {
-          desc = "Find text";
-        };
+        options.desc = "Find text";
       };
       "<leader>fR" = {
         action = "resume";
-        options = {
-          desc = "Resume";
-        };
+        options.desc = "Resume";
       };
       "<leader>fg" = {
         action = "oldfiles";
-        options = {
-          desc = "Recent";
-        };
+        options.desc = "Recent";
       };
       "<leader>fb" = {
         action = "buffers";
-        options = {
-          desc = "Buffers";
-        };
+        options.desc = "Buffers";
       };
       "<C-p>" = {
         action = "git_files";
-        options = {
-          desc = "Search git files";
-        };
+        options.desc = "Search git files";
       };
       "<leader>gc" = {
         action = "git_commits";
-        options = {
-          desc = "Commits";
-        };
+        options.desc = "Commits";
       };
       "<leader>gs" = {
         action = "git_status";
-        options = {
-          desc = "Status";
-        };
+        options.desc = "Status";
       };
       "<leader>sa" = {
         action = "autocommands";
-        options = {
-          desc = "Auto Commands";
-        };
+        options.desc = "Auto Commands";
       };
       "<leader>sb" = {
         action = "current_buffer_fuzzy_find";
-        options = {
-          desc = "Buffer";
-        };
+        options.desc = "Buffer";
       };
       "<leader>sc" = {
         action = "command_history";
-        options = {
-          desc = "Command History";
-        };
+        options.desc = "Command History";
       };
       "<leader>sC" = {
         action = "commands";
-        options = {
-          desc = "Commands";
-        };
+        options.desc = "Commands";
       };
       "<leader>sD" = {
         action = "diagnostics";
-        options = {
-          desc = "Workspace diagnostics";
-        };
+        options.desc = "Workspace diagnostics";
       };
       "<leader>sh" = {
         action = "help_tags";
-        options = {
-          desc = "Help pages";
-        };
+        options.desc = "Help pages";
       };
       "<leader>sH" = {
         action = "highlights";
-        options = {
-          desc = "Search Highlight Groups";
-        };
+        options.desc = "Search Highlight Groups";
       };
       "<leader>sk" = {
         action = "keymaps";
-        options = {
-          desc = "Keymaps";
-        };
+        options.desc = "Keymaps";
       };
       "<leader>sM" = {
         action = "man_pages";
-        options = {
-          desc = "Man pages";
-        };
+        options.desc = "Man pages";
       };
       "<leader>sm" = {
         action = "marks";
-        options = {
-          desc = "Jump to Mark";
-        };
+        options.desc = "Jump to Mark";
       };
       "<leader>so" = {
         action = "vim_options";
-        options = {
-          desc = "Options";
-        };
+        options.desc = "Options";
       };
       "<leader>sR" = {
         action = "resume";
-        options = {
-          desc = "Resume";
-        };
+        options.desc = "Resume";
       };
       "<leader>uC" = {
         action = "colorscheme";
-        options = {
-          desc = "Colorscheme preview";
-        };
+        options.desc = "Colorscheme preview";
       };
     };
   };
@@ -177,25 +129,19 @@
       mode = "n";
       key = "<leader>sd";
       action = "<cmd>Telescope diagnostics bufnr=0<cr>";
-      options = {
-        desc = "Document diagnostics";
-      };
+      options.desc = "Document diagnostics";
     }
     {
       mode = "n";
       key = "<leader>fe";
       action = "<cmd>Telescope file_browser<cr>";
-      options = {
-        desc = "File browser";
-      };
+      options.desc = "File browser";
     }
     {
       mode = "n";
       key = "<leader>fE";
       action = "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>";
-      options = {
-        desc = "File browser";
-      };
+      options.desc = "File browser";
     }
   ];
   extraConfigLua = /*lua*/''
